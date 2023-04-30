be22757dcd331bc6e4674dc959246efe02ab2e05
canberk <canberkozler0@gmail.com>
2023-04-30 10:52:25 +0300

    Proje dosyası ekle.

---
+++ DesignPatterns/DesignPatterns.cpp
@@ -0,0 +1,12 @@
+﻿// DesignPatterns.cpp: Uygulamanın giriş noktasını tanımlar.
+//
+
+#include "DesignPatterns.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
